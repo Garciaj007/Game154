f79a184c0b6300a657f7765d103ad40fe38d7c31
Juriel Garcia <jurielgarcia2010@gmail.com>
2018-03-17 01:04:39 -0400

    1.0.1

---
+++ Contra Final Project/Assets/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public Sprite background;
+
+    private Camera cam;
+    private Vector2 screenSize;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+        screenSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float xPos = cam.transform.position.x;
+
+        if(player.position.x < -background.bounds.extents.x + screenSize.x )
+        {
+            xPos = -background.bounds.extents.x + screenSize.x;
+        }
+        else if(player.position.x > background.bounds.extents.x - screenSize.x)
+        {
+            xPos = background.bounds.extents.x - screenSize.x;
+        } else 
+        {
+            xPos = player.position.x;
+        }
+
+        cam.transform.position = new Vector3(xPos, cam.transform.position.y, cam.transform.position.z);
+	}
+}
---
+++ Contra Final Project/Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent (typeof (SpriteRenderer))]
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Animator))]
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    private SpriteRenderer sp;
+    private Animator anim;
+
+    private float movement;
+    private bool jump;
+    private bool isGrounded = false;
+
+    public float speed = 10.0f;
+    public float jumpForce = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal") * speed;
+        jump = Convert.ToBoolean(Input.GetAxis("Jump"));
+
+        if(movement < 0)
+        {
+            sp.flipX = true;
+        } else
+        {
+            sp.flipX = false;
+        }
+
+        anim.SetBool("Moving", movement != 0);
+        anim.SetBool("Jumping", jump && isGrounded);
+        Debug.Log(movement);
+	}
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(movement, rigid.velocity.y);
+
+        if (jump && isGrounded)
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        } 
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            isGrounded = false;
+        }
+    }
+}
