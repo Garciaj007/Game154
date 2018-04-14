9e23767594e764b2fe8ad66950b0a4c0e5df21a5
Juriel Garcia <jurielgarcia2010@gmail.com>
2018-04-13 22:52:34 -0400

    1.0.9

---
+++ GameProgramming3Labs/Assets/Editor/AutoSave.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System;
+
+public class AutoSave : EditorWindow{
+
+    private bool autoSaveScene = true;
+    private bool showMessage = true;
+    private bool isStarted = false;
+    private int intervalScene;
+    private DateTime lastSaveTimeScene = DateTime.Now;
+
+    private string projectPath;
+    private string scenePath;
+    private Scene scene;
+
+    [MenuItem("Window/AutoSave")]
+    public static void ShowWindow()
+    {
+        GetWindow<AutoSave>();
+    }
+
+    private void OnEnable()
+    {
+        projectPath = Application.dataPath;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Info:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Saving to:", "" + projectPath);
+        EditorGUILayout.LabelField("Saving scene:", "" + scene.path);
+        GUILayout.Label("Options:", EditorStyles.boldLabel);
+        autoSaveScene = EditorGUILayout.BeginToggleGroup("Auto save", autoSaveScene);
+        intervalScene = EditorGUILayout.IntSlider("Interval (minutes)", intervalScene, 1, 15);
+        if (isStarted)
+        {
+            EditorGUILayout.LabelField("Last save:", "" + lastSaveTimeScene);
+        }
+        EditorGUILayout.EndToggleGroup();
+        showMessage = EditorGUILayout.BeginToggleGroup("Show Message", showMessage);
+        EditorGUILayout.EndToggleGroup();
+    }
+
+    private void Update()
+    {
+        scene = EditorSceneManager.GetActiveScene();
+        if (autoSaveScene)
+        {
+            if(DateTime.Now.Minute >= (lastSaveTimeScene.Minute + intervalScene) || DateTime.Now.Minute == 59 && DateTime.Now.Second == 59)
+            {
+                SaveScene();
+            }
+        } else
+        {
+            isStarted = false;
+        }
+    }
+
+    private void SaveScene()
+    {
+        EditorSceneManager.SaveScene(scene, scene.path);
+        lastSaveTimeScene = DateTime.Now;
+        isStarted = true;
+        if (showMessage)
+        {
+            Debug.Log("AutoSave saved: " + scene.path + " on " + lastSaveTimeScene);
+        }
+        GetWindow<AutoSave>().Repaint();
+    }
+}
---
+++ GameProgramming3Labs/Assets/Lab6/Scripts/Dialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+
+    public TextMeshPro name, sentences;
+
+    public void Display(DialogueItem item)
+    {
+        if(item != null)
+        {
+            name.text = item.name;
+            sentences.text = item.sentences;
+        }
+    }
+}
